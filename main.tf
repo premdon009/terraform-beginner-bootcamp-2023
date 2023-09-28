46dde58dae36c9dcaa6f0eb0f1f7c4a7cf5e469c
premdon009 <49059999+premdon009@users.noreply.github.com>
2023-09-28 22:04:42 +0530

    #13 Installed AWS terraform provider (#14)
    Added

--- main.tf
+++ main.tf
@@ -4,8 +4,19 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
   }
 }
+
+# provider "aws" {
+#   # Configuration options
+#   region     = AWS_DEFAULT_REGION
+#   access_key = AWS_ACCESS_KEY_ID
+#   secret_key = AWS_SECRET_ACCESS_KEY
+# }
 
 provider "random" {
   # Configuration options
@@ -13,10 +24,24 @@ provider "random" {
 
 
 resource "random_string" "bucket_name" {
-length           = 16
-special          = false
-#   override_special = "/@£$"
+length           = 60
+special          = true
+override_special = "-"
+upper = false
+
 }
+
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = random_string.bucket_name.result
+
+#   # tags = {
+#   #   Name        = "My bucket"
+#   #   Environment = "Dev"
+#   # }
+# }
+
+
 
 output "random_bucket_name" {
   value = random_string.bucket_name.id
