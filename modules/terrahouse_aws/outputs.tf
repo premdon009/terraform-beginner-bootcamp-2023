58e4d5dcc597711ef9978a306f42c1b05e62e7ae
premdon009 <49059999+premdon009@users.noreply.github.com>
2023-10-02 14:36:56 +0530

    #29 Enabled website hosting, uploaded index & err  html files (#30)

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -2,3 +2,14 @@ output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
 
+## You cant use it since The attribute "website_endpoint" is deprecated.
+# output "website_endpoint1" {
+
+#   description = "value of bucket endpoint from bucket resource"
+#   value = aws_s3_bucket.website_bucket.website_endpoint
+# }
+
+output "website_endpoint" {
+  description = "value of bucket endpoint from bucket website resource"
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
