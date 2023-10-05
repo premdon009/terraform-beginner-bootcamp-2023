3d1b13bd6100bfdf23e71f680b0964502aa1458b
Prem Kumar Raja <premdon009@gmail.com>
2023-10-05 15:43:05 +0000

    #35 Added TF block for adding multiple asset files to S3

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -91,3 +91,15 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
 }
+
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(var.assets_path, "*.{jpg, png, gif}")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "assets/${each.key}"
+  source = "${var.assets_path}/${each.key}"
+  etag = filemd5("${var.assets_path}/${each.key}")
+  lifecycle {
+    ignore_changes = [ etag ]
+    replace_triggered_by = [terraform_data.content_version.output]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,8 @@ variable "error_html_filepath" {
 variable "content_version" {
   type        = number
 }
+
+variable "assets_path" {
+  type  = string
+  description = "Path to assets folder"
+}
